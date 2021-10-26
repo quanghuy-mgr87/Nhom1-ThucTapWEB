e08631abca2fa2624dfe1132bdf3e0a89fbf6111
quanghuy-mgr87 <91078298+quanghuy-mgr87@users.noreply.github.com>
2021-10-26 16:35:03 +0700

    a

---
+++ MVC_PHP/Controller/admin/listPost.php
@@ -0,0 +1,9 @@
+<?php
+
+class ListPost
+{
+    public function __construct()
+    {
+        require('pages/post/list.php');
+    }
+}
---
+++ MVC_PHP/Controller/admin/post.php
@@ -0,0 +1,11 @@
+public function postList()
+{
+$sql = "SELECT * FROM posts";
+$result = $this->db->conn->query($sql);
+$list = array();
+while ($data = $result->fetch_array()) {
+$list[] = $data;
+}
+
+return $list;
+}
